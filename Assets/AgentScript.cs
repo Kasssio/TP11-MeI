e4861e4f7193279d56cbc4b5bb2ebcd653879494
Lucas Estrada <101399158+Kasssio@users.noreply.github.com>
2022-10-05 12:12:48 -0300

    terminado

---
+++ Assets/AgentScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AgentScript : MonoBehaviour
+{
+    public UnityEngine.AI.NavMeshAgent agent;
+    public Transform destination;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.destination = destination.position;
+    }
+}
